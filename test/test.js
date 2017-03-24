07a4e73366a77df7e20841a26d807cfbf2942b94
Josh Stupplebeen <jpstup@gmail.com>
2017-03-23 19:32:26 -0600

    working on testing

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var jsc = require("jsverify");
+
+var boolFnAppliedThrice = jsc.forall("bool -> bool", "bool", function(f, b) {
+    return f(f(f(b))) === f(b);
+});
+
+jsc.assert(boolFnAppliedThrice);
